0c2a0791dac5f21562c07ef9b440afdc2557be2c
KT-08 <kanikatari2808@gmail.com>
2026-05-03 17:03:25 +0530

    switched to huggingface inference API

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,29 @@
 from fastapi import FastAPI
 from database import collection
-from transformers import pipeline
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+import os
 
-emotion_analyzer = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
+HF_TOKEN = os.environ.get("HF_TOKEN")
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all (for development)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def analyze_emotion(text):
+    response = requests.post(
+        "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base",
+        headers={"Authorization": f"Bearer {HF_TOKEN}"},
+        json={"inputs": text}
+    )
+    result = response.json()
+    return result[0][0]['label']
 
 @app.get("/")
 def home():
@@ -31,18 +41,13 @@ class Entry(BaseModel):
 
 @app.post("/create-entry")
 def create_entry(entry: Entry):
-
-    result = emotion_analyzer(entry.content)
-    emotion = result[0]['label']
-
+    emotion = analyze_emotion(entry.content)
     data = {
         "title": entry.title,
         "content": entry.content,
         "emotion": emotion
     }
-
     collection.insert_one(data)
-
     return {
         "message": "Journal entry created",
         "emotion": emotion
@@ -50,28 +55,19 @@ def create_entry(entry: Entry):
 
 @app.get("/entries")
 def get_entries():
-
     entries = list(collection.find({}, {"_id": 0}))
-
     return {"entries": entries}
 
 @app.delete("/delete-entry/{title}")
 def delete_entry(title: str):
-
     result = collection.delete_one({"title": title})
-
     if result.deleted_count == 0:
         return {"message": "Entry not found"}
-
     return {"message": "Entry deleted successfully"}
 
 @app.put("/update-entry/{title}")
 def update_entry(title: str, new_content: str):
-
-    # Analyze new emotion
-    result_ai = emotion_analyzer(new_content)
-    new_emotion = result_ai[0]['label']
-
+    new_emotion = analyze_emotion(new_content)
     result = collection.update_one(
         {"title": title},
         {
@@ -81,12 +77,9 @@ def update_entry(title: str, new_content: str):
             }
         }
     )
-
     if result.matched_count == 0:
         return {"message": "Entry not found"}
-
     return {
         "message": "Entry updated successfully",
         "new_emotion": new_emotion
     }
-
