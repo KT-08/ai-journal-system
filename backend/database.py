7973d124d32234ad6a9d43fea766fac2d3c7eee3
KT-08 <kanikatari2808@gmail.com>
2026-05-03 16:15:34 +0530

    updated database.py to use environment variable

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
+import os
 
-MONGO_URL = "mongodb+srv://KT-08:aijournalsystem@cluster0.yghilni.mongodb.net/?appName=Cluster0"
+# MONGO_URL = "mongodb+srv://KT-08:aijournalsystem@cluster0.yghilni.mongodb.net/?appName=Cluster0"
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://KT-08:aijournalsystem@cluster0.yghilni.mongodb.net/?appName=Cluster0")
 
 client = MongoClient(MONGO_URL)
 
